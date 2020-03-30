9123ffbacccc3a74194f3db50f3e41d869de4c9c
hoanh <hoanh@vng.com.vn>
2020-03-30 10:20:03 +0700

    support specify page size and orientation when generate diagram

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ render:
 
 pdf:
 	rm -f cp.pdf
-	./generate.py "CorePayment.xlsx" > cp.drawio
+	./generate.py "CorePayment.xlsx" -po landscape > cp.drawio
 	/Applications/draw.io.app/Contents/MacOS/draw.io --export -f pdf -o cp.pdf cp.drawio
 	open cp.pdf
+
+ppr:
+	rm -f ppr.pdf
+	./generate.py "ppr.xlsx" > ppr.drawio
+	/Applications/draw.io.app/Contents/MacOS/draw.io --export -f pdf -o ppr.pdf ppr.drawio
+	open ppr.pdf
--- generate.py
+++ generate.py
@@ -4,17 +4,58 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 import xdrawio
 
+def get_page_dimention(page_size, page_orientation):
+    sizes = {
+        'A0': (9933, 14043),
+        'A1': (7016, 9933),
+        'A2': (4961, 7016),
+        'A3': (3508, 4961),
+        'A4': (2480, 3508),
+    }
+
+    ps = sizes[page_size]
+    if page_orientation == 'portrait':
+        return ps[0], ps[1]
+    elif page_orientation == 'landscape':
+        return ps[1], ps[0]
+    else:
+        return 0, 0
+
 def main():
     import argparse
     from xdrawio.layout import create_layout, layout_workgroup
     from xdrawio.datatypes import Page
 
 
-    parser = argparse.ArgumentParser(description='Render team structure to drawio file format.')
+    parser = argparse.ArgumentParser(
+        description='Render team structure to drawio file format.',
+        epilog='Enjoy!'
+        )
+    parser.version = "1.0"
     parser.add_argument('team_path', metavar='path', type=str,
                         help='path to xlsx file that contains team structure')
-    parser.add_argument('--debug', type=bool,
-                        help='debug flag, when enable only print debug data, not print drawio data')
+    parser.add_argument(
+        '-d',
+        '--debug',
+        type=bool,
+        help='debug flag, when enable only print debug data, not print drawio data'
+    )
+
+    parser.add_argument(
+        '-ps',
+        '--page-size',
+        type=str,
+        help='page size, possible values: A0, A1, A2, A3, A4',
+        default='A3'
+    )
+
+    parser.add_argument(
+        '-po',
+        '--page-orientation',
+        type=str,
+        help='page orientation, possible values: portrait, landscape',
+        default='portrait'
+    )
 
     args = parser.parse_args()
 
@@ -39,8 +80,16 @@ def main():
     # flat out
     all_items = page.flatten_tree()
 
+    w, h = get_page_dimention(args.page_size, args.page_orientation)
+    page = {
+        "width": w,
+        "height": h
+    }
     if not args.debug:
-        print(template.render(items=all_items, configs=d.configurations))
+        print(template.render(
+                items=all_items,
+                configs=d.configurations,
+                page=page))
 
 
 if __name__ == "__main__":
